5305deede766400a0941908905f3ddca9a496c12
Adrian Begic <adrianbegic1@gmail.com>
2021-09-19 21:55:55 -0400

    major

--- main.js
+++ main.js
@@ -1,26 +1,48 @@
-$(document).ready(function(){
-    if(window.innerWidth < 992) {
-      $("ul .fa-caret-down").click(function(e){
-        $(this).siblings('.dd').fadeToggle()
-      });
-    }
-  });
-  
-  
-  let toggleIcon = document.querySelector('.icon')
-  
-  toggleIcon.onclick = function() {
-    toggleIcon.classList.toggle('active')
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
   }
-  
-  let responsiveNavButton = document.querySelector('.icon'),
-      responsiveNavMenu = document.querySelector('nav#responsive')
-  
-  responsiveNavButton.addEventListener('click', toggleNav)
-  
-  function toggleNav(){
-    responsiveNavMenu.style.left === '0%' ? responsiveNavMenu.style.left = '-100%' : responsiveNavMenu.style.left = '0%'
+}
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
   }
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
+
   
   $(document).ready(function(){
     $('.your-class').slick({
